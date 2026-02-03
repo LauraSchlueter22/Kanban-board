0235fc79267ea84cd988ab0122bf8bce2fd6208a
Laura <lauraschlueter@ymail.com>
2026-02-03 12:44:24 -0800

    (feat): Add local storage

--- src/Task.tsx
+++ src/Task.tsx
@@ -7,9 +7,4 @@ export interface Task {
     column: ColumnId;
 }
 
-export const Tasks: Task[] = [
-  { id: 1, title: "Design login page", description: "Create forms for login, as well as add create account button", column: "todo" },
-  { id: 2, title: "Design new account page", description: "Add inputs and forms for new account", column: "todo" },
-  { id: 3, title: "Create API key", description: "Create API key for App", column: "inprogress" },
-  { id: 4, title: "Create company logo", description: "Create logo for app", column: "done" },
-];
+export const Tasks: Task[] = [];
